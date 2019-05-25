865143270968910fecf5f1953e08eb9f56ab5d05
nour <nour@Nours-MacBook-Pro.local>
2019-05-25 11:57:37 -0400

    finished the final build

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
 (() => {
 console.log('my JS is workin!!!');
 
+// VARIABLES GO HEREE
+allIcons = document.querySelectorAll(".indivIcon img");
+
+
+// FUNCTIONS GO HERE
+function logTheSVG() {
+		console.log(this.id);
+	}
+
+
+// EVENTS GO HERE
+
+// log
+allIcons.forEach(item=> {
+	item.addEventListener("click", logTheSVG);
+})
+
 })();
